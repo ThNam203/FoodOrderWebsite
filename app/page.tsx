ec7f3562bad480bcff7d4f4a4c040a7bed420702
ThNam203 <hthnam203@gmail.com>
2024-02-29 07:58:44 +0700

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,231 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
 
 export default function Home() {
+  const [activeCategory, setActiveCategory] = useState(1);
+
+  var data: any = {
+    categories: [
+      {
+        id: 1,
+        text: "All",
+        icon: "https://cdn-icons-png.flaticon.com/128/5110/5110796.png",
+      },
+      {
+        id: 2,
+        text: "Pizzaa",
+        icon: "https://cdn-icons-png.flaticon.com/128/1404/1404945.png",
+      },
+      {
+        id: 3,
+        text: "Asian",
+        icon: "https://cdn-icons-png.flaticon.com/128/4329/4329449.png",
+      },
+      {
+        id: 4,
+        text: "Burgers",
+        icon: "https://cdn-icons-png.flaticon.com/128/878/878052.png",
+      },
+      {
+        id: 5,
+        text: "Barbecue",
+        icon: "https://cdn-icons-png.flaticon.com/128/2946/2946598.png",
+      },
+      {
+        id: 6,
+        text: "Desserts",
+        icon: "https://cdn-icons-png.flaticon.com/128/4465/4465242.png",
+      },
+      {
+        id: 7,
+        text: "Thai",
+        icon: "https://cdn-icons-png.flaticon.com/128/197/197452.png",
+      },
+      {
+        id: 8,
+        text: "Sushi",
+        icon: "https://cdn-icons-png.flaticon.com/128/3183/3183425.png",
+      },
+    ],
+    foodItems: [
+      {
+        title: "Bagel Story",
+        image:
+          "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/25008305-442083466194421-4458779521922891776-n-1517333246.jpg?crop=1xw:1xh;center,top&resize=480:*",
+        prepTimeValue: "25 - 30",
+        prepTimeUnit: "min",
+        rating: 4.7,
+        cat1: "Deli",
+        cat2: "Bagels",
+        range: "$$",
+      },
+      {
+        title: "Dessert Rose",
+        image:
+          "https://images.mygoodtimes.in/wp-content/uploads/2018/12/08210525/Bakers-Copy.jpg",
+        prepTimeValue: "30 - 35",
+        prepTimeUnit: "min",
+        rating: 4.5,
+        cat1: "Cafes",
+        cat2: "Desserts",
+        range: "$",
+      },
+      {
+        title: "Barbecue Nation",
+        image:
+          "https://chowhound1.cbsistatic.com/thumbnail/370/0/chowhound1.cbsistatic.com/assets/2012/08/30453_RecipeImage_620x413_grilled_chicken_nectarine.jpg",
+        prepTimeValue: "40 - 60",
+        prepTimeUnit: "min",
+        rating: 4.6,
+        cat1: "Barbecue",
+        cat2: "Chicken",
+        range: "$$$",
+      },
+    ],
+    cartItems: [
+      {
+        title: "BBQ Burger",
+        image:
+          "https://hips.hearstapps.com/pop.h-cdn.co/assets/cm/15/05/54ca71fb94ad3_-_5summer_skills_burger_470_0808-de.jpg?crop=1xw:1.0xh;center,top&resize=480:*",
+        quantity: 1,
+        price: 14.99,
+      },
+      {
+        title: "French Fries",
+        image:
+          "https://recipes.timesofindia.com/thumb/54659021.cms?width=1200&height=1200",
+        quantity: 1,
+        price: 9.99,
+      },
+      {
+        title: "Cheese Sauce",
+        image:
+          "https://www.pepperscale.com/wp-content/uploads/2017/10/spicy-nacho-cheese.jpeg",
+        quantity: 1,
+        price: 0.99,
+      },
+    ],
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <>
+      <div className="flex" v-cloak id="dribbleShot">
+        <div className="main px-8 border-gray-200">
+          <div className="h-12 mt-8 flex items-center justify-between">
+            <div className="flex items-center rounded-md bg-gray-100 self-stretch px-4 w-2/3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20px"
+                height="20px"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fill="black"
+                  d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0a5.5 5.5 0 0 1 11 0"
+                />
+              </svg>
+              <input
+                type="text"
+                className="px-4 self-stretch bg-transparent flex-grow outline-none"
+                placeholder="Search"
+              />
+            </div>
+            <div className="flex flex-row items-center gap-2 hover:cursor-pointer">
+              <img
+                src="https://scontent.fsgn19-1.fna.fbcdn.net/v/t1.6435-1/89355819_802321806918041_2820306896441835520_n.jpg?stp=dst-jpg_p240x240&_nc_cat=106&ccb=1-7&_nc_sid=2b6aad&_nc_ohc=mGJ07GAwJPIAX-HmCpl&_nc_ht=scontent.fsgn19-1.fna&oh=00_AfDzBpS8oC1vm-lKKyG65r9dHO5IqZrn36HGZ17stH9nXg&oe=6606CBD4"
+                className="w-10 h-10 rounded-full"
+              />
+              <p className="text-sm">Huynh Nam</p>
+            </div>
+          </div>
+          <div className="grid grid-cols-6 grid-rows-2 mt-12 rounded-lg gap-1">
+            <img
+              src="https://cf.shopee.vn/file/vn-50009109-93074cd7272fcd06fc514ef80e8aa20f_xxhdpi"
+              alt="banner 1"
+              className="col-span-4 row-span-2 h-full object-cover rounded-md"
             />
-          </a>
+            <img
+              src="https://cf.shopee.vn/file/vn-50009109-ed6696a2bea64ffee99377b73c44d5e8_xhdpi"
+              alt="banner 2"
+              className="col-span-2 rounded-md"
+            />
+            <img
+              src="https://cf.shopee.vn/file/vn-50009109-c5335039e1b1aab390cc29f3446908fc_xhdpi"
+              alt="banner 2"
+              className="col-span-2 rounded-md"
+            />
+          </div>
+          <div className="flex mt-8 justify-center">
+            <h3 className="text-3xl font-bold">Categories</h3>
+          </div>
+          <div className="grid grid-cols-9 gap-6 mt-12">
+            {data.categories.map((cat: any, idx: number) => (
+              <div
+                key={idx}
+                onClick={() => setActiveCategory(cat.id)}
+                className={`border rounded-full p-2 flex flex-col items-center shadow-xl cursor-pointer transition-colors duration-700 ease-in-out ${
+                  cat.id === activeCategory ? "bg-yellow-400" : ""
+                }`}
+              >
+                <div className="rounded-full p-2 bg-white">
+                  <img className="h-12 w-12" src={cat.icon} alt="" />
+                </div>
+                <p className="mt-3 mb-10 font-bold text-xs">{cat.text}</p>
+              </div>
+            ))}
+            <button className="h-10 rounded-lg self-center hover:shadow-xl flex justify-center items-center bg-gray-100 p-3 ml-auto">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20px"
+                height="20px"
+                viewBox="0 0 24 24"
+              >
+                <path fill="black" d="M12.6 12L8 7.4L9.4 6l6 6l-6 6L8 16.6z" />
+              </svg>
+            </button>
+          </div>
+          <div className="mt-12 grid grid-cols-3 gap-10">
+              {data.foodItems.map((food: any, index: number) => (
+                <div className="flex flex-row">
+                  <div
+                    key={index}
+                    className="shadow-lg relative h-40 custom-rounded"
+                    style={{
+                      backgroundSize: "cover",
+                      backgroundRepeat: "no-repeat",
+                      backgroundImage: `url(${food.image})`,
+                    }}
+                  >
+                    <div className="absolute bottom-0 left-0 w-1/3 bg-gray-200 rounded-tr-lg p-2 text-center text-xs">
+                      <span className="font-bold">{food.prepTimeValue}</span>{" "}
+                      {food.prepTimeUnit}
+                    </div>
+                  </div>
+                  <p className="mt-4 font-medium">{food.title}</p>
+                  <div className="mt-2 flex items-center">
+                    <span className="text-xs">
+                      <i className="fa fa-star"></i> {food.rating}
+                    </span>
+                    <span className="text-gray-600 font-hairline text-xs mx-4">
+                      {food.cat1}
+                    </span>
+                    <span className="text-gray-600 font-hairline text-xs mx-4">
+                      {food.cat2}
+                    </span>
+                    <span className="text-gray-600 font-hairline text-xs mx-4">
+                      {food.range}
+                    </span>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
+      <div className="flex h-10">
+        <div className="flex-grow px-16 main border-r border-gray-200"></div>
+        <div className="sidebar bg-gray-100"></div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
 }
---
+++ components/sidebar.tsx
@@ -0,0 +1,132 @@
+"use client";
+
+import style from "@/styles/sidebar.module.css";
+import { useState } from "react";
+import { twMerge } from "tailwind-merge";
+
+export default function Sidebar({
+  onSidebarToggle,
+  intitalState,
+}: {
+  intitalState: boolean;
+  onSidebarToggle: () => void;
+}) {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(intitalState);
+
+  return (
+    <div
+      className={twMerge(
+        style["l-navbar"],
+        isSidebarOpen ? style.expander : ""
+      )}
+      id="navbar"
+    >
+      <nav className={style.nav}>
+        <div>
+          <div className={style["nav__brand"]}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="3rem"
+              height="3rem"
+              viewBox="0 0 24 24"
+              className={style["nav__toggle"]}
+              id="nav-toggle"
+              onClick={() => {
+                onSidebarToggle();
+                setIsSidebarOpen(!isSidebarOpen);
+              }}
+            >
+              <path
+                fill="white"
+                d="M4 17.27v-1h16v1zm0-4.77v-1h16v1zm0-4.77v-1h16v1z"
+              />
+            </svg>
+            <a href="#" className={style["nav__logo"]}>
+              FFOOD
+            </a>
+          </div>
+          <div className={style["nav__list"]}>
+            <a
+              href="#"
+              className={twMerge(style["nav__link"], style["active"])}
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 24 24"><path fill="white" d="M13.5 9V4H20v5zM4 12V4h6.5v8zm9.5 8v-8H20v8zM4 20v-5h6.5v5zm1-9h4.5V5H5zm9.5 8H19v-6h-4.5zm0-11H19V5h-4.5zM5 19h4.5v-3H5zm4.5-3"/></svg>
+              <span className={style["nav__name"]}>Dashboard</span>
+            </a>
+            <a href="#" className={style["nav__link"]}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20px"
+                height="20px"
+                viewBox="0 0 24 24"
+              >
+                <g fill="none" stroke="white">
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M14.5 21v-5a1 1 0 0 0-1-1h-3a1 1 0 0 0-1 1v5"
+                  />
+                  <path d="M5 11v6c0 1.886 0 2.828.586 3.414C6.172 21 7.114 21 9 21h6c1.886 0 2.828 0 3.414-.586C19 19.828 19 18.886 19 17v-6M4.621 4.515c.182-.728.273-1.091.544-1.303C5.437 3 5.812 3 6.562 3h10.876c.75 0 1.125 0 1.397.212c.27.212.362.575.544 1.303l1.203 4.814c.097.388.146.581.135.739a1 1 0 0 1-.69.883c-.15.049-.354.049-.763.049c-.533 0-.8 0-1.023-.052a2 2 0 0 1-1.393-1.18c-.089-.212-.132-.47-.217-.983c-.024-.144-.036-.216-.05-.235a.1.1 0 0 0-.162 0c-.014.019-.026.09-.05.235l-.081.489A2 2 0 0 1 14.352 11h-.204a2 2 0 0 1-1.936-1.726l-.081-.49c-.024-.143-.036-.215-.05-.234a.1.1 0 0 0-.162 0c-.014.019-.026.09-.05.235l-.081.489A2 2 0 0 1 9.852 11h-.204A2 2 0 0 1 7.73 9.374a1.386 1.386 0 0 1-.018-.1l-.081-.49c-.024-.143-.036-.215-.05-.234a.1.1 0 0 0-.162 0c-.014.019-.026.09-.05.235c-.085.514-.128.77-.217.983a2 2 0 0 1-1.392 1.18C5.536 11 5.27 11 4.736 11c-.409 0-.613 0-.763-.049a1 1 0 0 1-.69-.883c-.01-.158.038-.351.135-.739z" />
+                </g>
+              </svg>
+              <span className={style["nav__name"]}>Browse</span>
+            </a>
+
+            <a href="#" className={twMerge(style["nav__link"])}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20px"
+                height="20px"
+                viewBox="0 0 24 24"
+              >
+                <g fill="none" stroke="white" stroke-width="1.5">
+                  <path d="M3.864 16.455c-.858-3.432-1.287-5.147-.386-6.301C4.378 9 6.148 9 9.685 9h4.63c3.538 0 5.306 0 6.207 1.154c.901 1.153.472 2.87-.386 6.301c-.546 2.183-.818 3.274-1.632 3.91c-.814.635-1.939.635-4.189.635h-4.63c-2.25 0-3.375 0-4.189-.635c-.814-.636-1.087-1.727-1.632-3.91Z" />
+                  <path
+                    d="m19.5 9.5l-.71-2.605c-.274-1.005-.411-1.507-.692-1.886A2.5 2.5 0 0 0 17 4.172C16.56 4 16.04 4 15 4M4.5 9.5l.71-2.605c.274-1.005.411-1.507.692-1.886A2.5 2.5 0 0 1 7 4.172C7.44 4 7.96 4 9 4"
+                    opacity="0.5"
+                  />
+                  <path d="M9 4a1 1 0 0 1 1-1h4a1 1 0 1 1 0 2h-4a1 1 0 0 1-1-1Z" />
+                </g>
+              </svg>
+              <span className={style["nav__name"]}>Your Cart</span>
+            </a>
+
+            <a href="#" className={twMerge(style["nav__link"])}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20px"
+                height="20px"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="white"
+                  d="m12 19.654l-.758-.685q-2.448-2.236-4.05-3.829q-1.602-1.592-2.529-2.808q-.926-1.217-1.295-2.201Q3 9.146 3 8.15q0-1.908 1.296-3.204Q5.592 3.65 7.5 3.65q1.32 0 2.475.675T12 6.288Q12.87 5 14.025 4.325T16.5 3.65q1.908 0 3.204 1.296Q21 6.242 21 8.15q0 .996-.368 1.98q-.369.985-1.295 2.202q-.927 1.216-2.52 2.808q-1.592 1.593-4.06 3.83zm0-1.354q2.4-2.17 3.95-3.716q1.55-1.547 2.45-2.685t1.25-2.015Q20 9.006 20 8.15q0-1.5-1-2.5t-2.5-1q-1.194 0-2.204.682q-1.01.681-1.808 2.053h-.976q-.818-1.39-1.818-2.063q-1-.672-2.194-.672q-1.48 0-2.49 1Q4 6.65 4 8.15q0 .856.35 1.734t1.25 2.015t2.45 2.675Q9.6 16.112 12 18.3m0-6.825"
+                />
+              </svg>
+              <span className={style["nav__name"]}>Favorites</span>
+            </a>
+            <a href="#" className={twMerge(style["nav__link"])}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 24 24"><path fill="white" d="M11.962 20q-3.047 0-5.311-1.99Q4.387 16.022 4.03 13h1.011q.408 2.58 2.351 4.29Q9.337 19 11.962 19q2.925 0 4.962-2.037T18.962 12q0-2.925-2.038-4.963T11.962 5q-1.552 0-2.918.656q-1.365.656-2.41 1.806h2.481v1H4.962V4.308h1v2.388q1.16-1.273 2.718-1.984Q10.238 4 11.962 4q1.663 0 3.118.626q1.455.626 2.542 1.713t1.714 2.543q.626 1.455.626 3.118q0 1.663-.626 3.118q-.626 1.455-1.714 2.543q-1.087 1.087-2.542 1.713q-1.455.626-3.118.626m3.203-4.146l-3.646-3.646V7h1v4.792l3.354 3.354z"/></svg>
+              <span className={style["nav__name"]}>History</span>
+            </a>
+          </div>
+        </div>
+
+        <a href="#" className={twMerge(style["nav__link"], "hover:bg-red-400")}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20px"
+            height="20px"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="white"
+              d="M5.615 20q-.69 0-1.152-.462Q4 19.075 4 18.385V5.615q0-.69.463-1.152Q4.925 4 5.615 4h6.404v1H5.615q-.23 0-.423.192Q5 5.385 5 5.615v12.77q0 .23.192.423q.193.192.423.192h6.404v1zm10.847-4.462l-.702-.719l2.319-2.319H9.192v-1h8.887l-2.32-2.32l.703-.718L20 12z"
+            />
+          </svg>
+          <span className={style["nav__name"]}>Log Out</span>
+        </a>
+      </nav>
+    </div>
+  );
+}
